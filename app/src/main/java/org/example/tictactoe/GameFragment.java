2dfe944ba79435100c3ecbe812a7dac81967347f
Crockett Hopper <dachaeon@me.com>
2016-10-01 08:49:18 -0500

    working on game fragment

--- app/src/main/java/org/example/tictactoe/GameFragment.java
+++ app/src/main/java/org/example/tictactoe/GameFragment.java
@@ -15,6 +15,21 @@ import java.util.Set;
 
 public class GameFragment extends Fragment {
     // Data structures go here...
+    static private int mLargeIds[] = {R.id.large1, R.id.large2, R.id.large3,
+            R.id.large4, R.id.large5, R.id.large6, R.id.large7, R.id.large8,
+            R.id.large9,};
+    static private int mSmallIds[] = {R.id.small1, R.id.small2, R.id.small3,
+            R.id.small4, R.id.small5, R.id.small6, R.id.small7, R.id.small8,
+            R.id.small9,};
+    private Tile mEntireBoard = new Tile(this);
+    private Tile mLargeTiles[] = new Tile[9];
+    private Tile mSmallTiles[][] = new Tile[9][9];
+    private Tile.Owner mPlayer = Tile.Owner.X;
+    private Set<Tile> mAvailable = new HashSet<Tile>();
+    private int mLastLarge;
+    private int mLastSmall;
+
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
